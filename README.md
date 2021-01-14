b17f2f1e9e645b9c1f393ca4ebe6a5a8e062d9cc
METENG Arthur <arthur.meteng@gmail.com>
2021-01-14 12:41:43 +0100

    :tada: setup Flask Server

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # LA_Project
+Welcome to the Flask Server of our application.
+In order to run the project you will need to install Python and Flask.
+When this is done run the commande python app.py to run the server.
---
+++ app.py
@@ -0,0 +1,38 @@
+from flask import Flask, render_template, request, make_response
+import json
+from time import time
+
+app=Flask(__name__)
+
+global dataList
+dataList = []
+
+
+@app.route("/", methods=['GET', 'POST'])
+def home():
+    if request.method == "GET":
+        return render_template("app.html")        
+
+@app.route("/data", methods=['GET', 'POST'])
+def datas():
+    if request.method == "GET":
+        if len(dataList) > 0:
+            print(len(dataList))
+            response = make_response(json.dumps(dataList.pop(0)))
+            response.content_type = 'application/json'
+            return response
+        else: return make_response()
+    else:
+        print(request.json)
+        data = {
+            'time' : time() * 1000,
+            'beat' : request.json['temperature'],
+        }
+        dataList.append(data)
+        print(dataList)
+        return make_response("ACK")
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
